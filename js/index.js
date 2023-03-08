2151e7cbc7c97a5bb728861acff35768bd0ee8db
Viktor Bojda <viktorbojda17@gmail.com>
2023-03-08 22:57:52 +0100

    Add top10 and detail page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 $(function() {
-    $("#table-winners").DataTable();
+    $("#table-winners").DataTable(
+        {
+            "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
+            "order": [],
+            "columnDefs": 
+            [
+                { 
+                    "orderable": false,
+                    "targets": [1, 6]  
+                },
+                {
+                    "target": 0,
+                    "visible": false,
+                    "searchable": false
+                }
+            ],
+            "createdRow": function( row, data, dataIndex ) {
+                let $fullName = $(row).find('td:eq(0), td:eq(1)');
+                $fullName.on("click", function() {
+                    location.href = `./detail.php?id=${data[0]}`;
+                });
+                }
+        }
+    );
 });
--- top-10.php
+++ top-10.php
@@ -10,17 +10,19 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql = "SELECT
-                person.id,
-                person.name,
-                person.surname,
-                game.year,
-                CONCAT(game.city, ', ', game.country) AS location,
-                game.type,
-                placement.discipline
+                CONCAT(person.name, ' ', person.surname) AS fullName,
+                COUNT(*) AS goldCount
             FROM
-                person
-            LEFT JOIN placement ON person.id = placement.person_id
-            INNER JOIN game ON placement.game_id = game.id";
+                placement
+            INNER JOIN person ON placement.person_id = person.id
+            WHERE
+                placement.placing = 1
+            GROUP BY
+                FullName
+            ORDER BY
+                GoldCount
+            DESC
+            LIMIT 10";
 
     $results = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $err) {
@@ -35,9 +37,8 @@ try {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prehľad medailistov</title>
+    <title>Top 10</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="./css/basic.css">
 </head>
 
@@ -58,39 +59,29 @@ try {
             </nav>
             <div class="collapse" id="nav-toggle">
                 <div class="row dark-blue-color mx-0">
-                    <a class="col-12 col-md-6 py-3 nav-button-active d-flex justify-content-center" href="#">Prehľad medailistov</a>
-                    <a class="col-12 col-md-6 py-3 d-flex justify-content-center" href="./top-10.php">Top 10</a>
+                    <a class="col-12 col-md-6 py-3 d-flex justify-content-center" href="./index.php">Prehľad medailistov</a>
+                    <a class="col-12 col-md-6 py-3 nav-button-active d-flex justify-content-center" href="#">Top 10</a>
                 </div>
             </div>
         </div>
 
         <div class="page-content p-3">
-            <h2 class="pb-3">Slovenskí olympijskí medailisti</h2>
+            <h2 class="pb-3">10 najúspešnejších slovenských olympionikov</h2>
 
             <div class="table-responsive">
                 <table id="table-winners" class="table">
                     <thead>
                         <tr>
-                            <td>ID</td>
                             <td>Meno</td>
-                            <td>Priezvisko</td>
-                            <td>Rok</td>
-                            <td>Miesto</td>
-                            <td>Typ</td>
-                            <td>Disciplína</td>
+                            <td>Počet zlatých medailí</td>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
                         foreach ($results as $result) {
                             echo "<tr>
-                                    <td>" . $result["id"] . "</td>
-                                    <td>" . $result["name"] . "</td>
-                                    <td>" . $result["surname"] . "</td>
-                                    <td>" . $result["year"] . "</td>
-                                    <td>" . $result["location"] . "</td>
-                                    <td>" . $result["type"] . "</td>
-                                    <td>" . $result["discipline"] . "</td>
+                                    <td>" . $result["fullName"] . "</td>
+                                    <td>" . $result["goldCount"] . "</td>
                                 </tr>";
                         }
                         ?>
@@ -100,11 +91,7 @@ try {
         </div>
         
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.3/js/dataTables.bootstrap5.min.js"></script>
-    <script src="./js/index.js"></script>
 </body>
 
 </html>
